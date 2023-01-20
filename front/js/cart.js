02d6b99a829216d1937fd01cb4affa49d632154f
El Ghalbzouri Adnan <alghalbzouri@santeformapro.com>
2023-01-20 15:59:02 +0100

    modification front && bac

---
+++ front/js/cart.js
@@ -0,0 +1,440 @@
+
+//Commentaire index.html :
+
+
+/* 
+<article class="cart__item" data-id="{product-ID}" data-color="{product-color}">
+    <div class="cart__item__img">
+        <img src="../images/product01.jpg" alt="Photographie d'un canapé">
+    </div>
+
+
+    <div class="cart__item__content">
+        <div class="cart__item__content__description">
+            <h2>Nom du produit</h2>
+            <p>Vert</p>
+            <p>42,00 €</p>
+        </div>
+
+
+       
+       
+        <div class="cart__item__content__settings">
+            <div class="cart__item__content__settings__quantity">
+                <p>Qté : </p>
+                <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="42">
+            </div>
+
+
+
+            <div class="cart__item__content__settings__delete">
+                <p class="deleteItem">Supprimer</p>
+            </div>
+        </div>
+    </div>
+</article>
+    2       84,00
+    ci est un message d'erreur
+*/
+
+
+//Commentaire Produit n°1 Description :
+
+/* {
+    "colors": [
+      "Blue",
+      "White",
+      "Black"
+    ],
+    
+    "_id": "107fb5b75607497b96722bda5b504926",
+    "name": "Kanap Sinopé",
+    "price": 1849,
+    "imageUrl": "http://localhost:3000/images/kanap01.jpeg",
+    "description": "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    "altTxt": "Photo d'un canapé bleu, deux places"
+  }, 
+  
+*/
+
+
+
+//LE LOCALE STORAGE :
+        
+//Déclaration de la variable "localStorageProducts" dans laquelles on met les key et les values qui sont dans le local stockage :
+let localStorageProducts = JSON.parse(localStorage.getItem("produits"));    
+
+
+// Variable pour stocker les id de chaque articles présents dans le panier :
+    let products = [];
+
+
+    // Condition de vérification si le panier existe et ou est vide et modification texte :
+    if (localStorageProducts == null || localStorageProducts.length == 0) {
+        document.querySelector('h1').textContent = '🛒 Le panier est vide 🛒 !';
+        document.querySelector('.cart__price').innerHTML = `<p>Total (<span id="totalQuantity">0</span> articles) : <span id="totalPrice">0</span> €</p>`;
+    }
+    
+    else{
+        document.querySelector('h1').textContent = '🛒 Voici votre panier 🛒 ';
+    };
+    
+
+    // Création d'une boucle for of dans laquelle ont injecte notre code grâce à un innerHTML et affichage des données :
+    
+    let i = 0;
+    
+    for (product of localStorageProducts) {
+
+        //requête Fetch : 
+        fetch('http://localhost:3000/api/products/' + product.id)
+        .then( (response) => response.json())
+        .then( (data) => {
+
+            //Création du tableau pour les produits à envoyer au serveur
+            products.push(product.id);
+
+
+            localStorageProducts[i].imageUrl = data.imageUrl;
+            localStorageProducts[i].altTxt = data.altTxt;
+            localStorageProducts[i].name = data.name;
+            localStorageProducts[i].price = data.price;
+
+
+            document.querySelector('#cart__items').innerHTML += `<article class="cart__item" data-id= ${localStorageProducts[i].id}  data-color= ${localStorageProducts[i].colors}>
+            <div class="cart__item__img">
+                <img src=${localStorageProducts[i].imageUrl} alt=${localStorageProducts[i].altTxt}>
+            </div>
+            <div class="cart__item__content">
+                <div class="cart__item__content__description">
+                    <h2>${localStorageProducts[i].name}</h2>
+                    <p>Couleur du produit : ${localStorageProducts[i].colors}</p>
+                    <p>Prix unitaire : ${localStorageProducts[i].price} €</p>
+                </div>
+                <div class="cart__item__content__settings">
+                    <div class="cart__item__content__settings__quantity">
+                        <p> Qté : ${localStorageProducts[i].quantity} </p>
+                        <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value=${localStorageProducts[i].quantity}>
+                    </div>
+                    <div class="cart__item__content__settings__delete">
+                        <p class="deleteItem">Supprimer</p>
+                    </div>
+                </div>
+            </div>
+        </article>`;
+
+        TotalPriceQuantity();
+        deleteProduct();
+        modifValue();
+
+        i++;
+
+    });
+    
+}
+
+
+//Déclaration d'une const avec une fonction TotalPriceQuantity qui vas afficher la quantity et le price total des produits :
+const TotalPriceQuantity = () => {
+        
+    // quantityTotalCalcul qui contient la quantity de chaque articles qui est dans le local storage :
+    var quantityTotalCalcul = 0;
+
+    // priceTotalCalcul qui contient la price de chaque articles qui est dans le local storage :
+    var priceTotalCalcul = 0;  
+
+    for (let i = 0; i < localStorageProducts.length; i++) {
+
+        //Déclaration de la variable quantityProduitDansLePanier dans laquelle ont vas chercher la quantity de tout les articles et que l'on met dans quantityTotalCalcul :
+        let quantityProduitDansLePanier = localStorageProducts[i].quantity;
+        quantityTotalCalcul += parseInt(quantityProduitDansLePanier);
+
+        //Déclaration de la variable priceProduitDansLePanier dans laquelle ont vas chercher le price de chaque articles et que l'on met dans priceTotalCalcul :
+        let priceProduitDansLePanier = localStorageProducts[i].price * localStorageProducts[i].quantity;
+        priceTotalCalcul += priceProduitDansLePanier;
+        
+    }
+    
+    //Affichage des résultat grâce à innerHtml : 
+    document.querySelector('.cart__price').innerHTML = `<p>Total (<span id="totalQuantity">${quantityTotalCalcul}</span> articles) : <span id="totalPrice">${priceTotalCalcul}</span> €</p>`;
+}
+
+
+
+
+//Création function  modifValue qui va changer la quantity des articles :
+function modifValue () {
+
+let inputQuantity = Array.from(document.querySelectorAll(".cart__item__content__settings__quantity input"));
+let valueQuantity = Array.from(document.querySelectorAll('.itemQuantity'));
+
+
+//Boucle for en vas chercher tout les input dans lequelle on effectue un addEventListener pour changer la value des articles :
+    for (let i = 0; i < inputQuantity.length; i++) {
+
+        inputQuantity[i].addEventListener("change", () => {
+        
+        // Copie du tableau localStorageProducts dans le tableau tabUpdate :
+        tabUpdate = localStorageProducts;
+            
+        //Création d'une boucle for pour supprimer dans le local storage les valeur altxt, imageUrl, name et price : 
+        for (let i = 0; i < tabUpdate.length; i++) { 
+        
+                delete tabUpdate[i].altTxt;
+                delete tabUpdate[i].imageUrl;
+                delete tabUpdate[i].name;
+                delete tabUpdate[i].price; 
+        }
+            
+        //On modifie la quantité d'un élément à chaque index [i] du tableau écouté :
+            tabUpdate[i].quantity = valueQuantity[i].value;
+
+        //Mise à jour du local storage :
+            localStorage.setItem("produits", JSON.stringify(tabUpdate));
+
+        //Rafraîchissement de la page :
+            window.location.reload();
+
+            TotalPriceQuantity();
+        });
+    }
+}
+
+
+
+
+
+/******************************** SUPPRESSION DES ARTICLES****************************/
+
+
+
+
+// Fonction de suppression des articles :
+function deleteProduct() {
+
+// Récupération boutons supprimer et transformation en tableau avec Array.from :
+let btn_supprimer = Array.from(document.querySelectorAll(".deleteItem"));
+
+// Nouveau tableau pour récupérer le tableau localStorageProducts existant et contrôler les suppression :
+let tabDelete = [];
+  for (let i = 0; i < btn_supprimer.length; i++) {
+
+    // Écoute d'évènements au click sur le tableau des boutons supprimer
+    btn_supprimer[i].addEventListener("click", () => {
+
+      // Suppression de l'article visuellement sur la page
+      btn_supprimer[i].style.display = "none";
+
+      // Copie du tableau localStorageProducts dans le tableau tabControlDelete
+      tabDelete = localStorageProducts;
+
+    
+    //Création d'une boucle for pour supprimer dans le local storage les valeur altxt, imageUrl, name et price l'orsque que l'ont supprime un article : 
+
+      for (let i = 0; i < tabDelete.length; i++) { 
+        
+        delete tabDelete[i].altTxt;
+        delete tabDelete[i].imageUrl;
+        delete tabDelete[i].name;
+        delete tabDelete[i].price;
+         
+    }
+
+      
+      // Array.prototype.splice() supprime un élément à chaque index [i] du tableau écouté
+      tabDelete.splice([i], 1);
+      
+      // Mise à jour du local storage
+      localStorageProducts = localStorage.setItem("produits", JSON.stringify(tabDelete));
+      
+
+      // Rafraîchissement de la page
+      window.location.reload();
+
+      
+    });
+  }
+}
+
+
+
+/*************************************  LE FORMULAIRE ********************************/
+
+
+//Sélection du bouton commander :
+let btnSendForm = document.querySelector('#order');
+
+
+//Écoute du bouton commander sur le click pour pouvoir contrôler, valider et ennoyer le formulaire et les produits au back-end :
+btnSendForm.addEventListener('click', (e) => {
+e.preventDefault();
+
+
+//Récupération des valeur du formulaire :
+const contact = {
+    firstName : document.querySelector("#firstName").value,
+    lastName : document.querySelector("#lastName").value,
+    address : document.querySelector("#address").value,
+    city : document.querySelector("#city").value,
+    email : document.querySelector("#email").value,
+    
+};
+    
+
+/******************************** GESTION DU FORMULAIRE ****************************/
+    
+
+    function firstNameControle () {     
+        //Regex pour le contrôle des champs Prénom :
+        const firstName = contact.firstName;  
+        let inputFirstName = document.querySelector("#firstName");
+        if (/^([A-Za-z\s]{3,20})?([-]{0,1})?([A-Za-z]{3,20})$/.test(firstName)) {
+            inputFirstName.style.border = "solid 2px green";
+            document.querySelector("#firstNameErrorMsg").textContent = "";
+            return true;
+        } 
+        
+        else {
+            inputFirstName.style.border = "solid 2px red";
+            document.querySelector("#firstNameErrorMsg").textContent = "Champ Prénom de formulaire invalide, ex: Bernard";
+            return false;
+        }
+        
+    }
+    
+
+
+    function lastNameControle () {     
+        //Regex pour le contrôle des champs Nom :
+        const lastName = contact.lastName; 
+        let inputLastName = document.querySelector("#lastName"); 
+        if (/^([A-Za-z\s]{3,20})?([-]{0,1})?([A-Za-z]{3,20})$/.test(lastName)) {
+            inputLastName.style.border = "solid 2px green";
+            document.querySelector("#lastNameErrorMsg").textContent = "";
+            return true;
+        } 
+                
+        else {
+            inputLastName.style.border = "solid 2px red";
+            document.querySelector("#lastNameErrorMsg").textContent = "Champ Nom de formulaire invalide, ex: Durand";
+            return false;
+        }
+            
+    }
+
+
+
+    function addressControl () {     
+        // Regex pour le contrôle des champs adresse :
+        const adresse = contact.address;  
+        let inputAddress = document.querySelector("#address");
+        if (/^[A-Za-z0-9\s]{5,100}$/.test(adresse)) {
+            inputAddress.style.border = "solid 2px green";
+            document.querySelector("#addressErrorMsg").textContent = "";
+            return true;
+        } 
+        
+        else {
+            inputAddress.style.border = "solid 2px red";
+            document.querySelector("#addressErrorMsg").textContent = "Champ Adresse de formulaire invalide, ex: 50 rue de la paix";
+            return false;
+        }
+        
+    }
+
+
+
+    
+    function cityControl () {     
+        //Regex pour le contrôle des champs Ville :
+        const city = contact.city;  
+        let inputCity = document.querySelector("#city");
+        if (/^([A-Za-z\s]{3,20})?([-]{0,1})?([A-Za-z]{3,20})$/.test(city)) {
+            inputCity.style.border = "solid 2px green";
+            document.querySelector("#cityErrorMsg").textContent = "";
+            return true;
+        } 
+        
+        else {
+            inputCity.style.border = "solid 2px red";
+            document.querySelector("#cityErrorMsg").textContent = "Champ Ville de formulaire invalide, ex: Bordeaux";
+            return false;
+        }
+        
+    }
+
+
+
+
+    function emailControle () {     
+        //Regex pour le contrôle des champs Email :
+        const email = contact.email;  
+        let inputMail = document.querySelector("#email");
+        if (/^[_a-z0-9-]+(.[_a-z0-9-]+)*@[a-z0-9-]+(.[a-z0-9-]+)*(.[a-z]{2,4})$/.test(email)) {
+            inputMail.style.border = "solid 2px green";
+            document.querySelector("#emailErrorMsg").textContent = "";
+            return true;
+        } 
+        
+        else {
+            inputMail.style.border = "solid 2px red";
+            document.querySelector("#emailErrorMsg").textContent = "Champ Email de formulaire invalide, ex: example@contact.fr";
+            return false;
+        }
+        
+    }
+
+
+
+
+    //Contrôle validité formulaire avant envoie dans le locale storage : 
+    if (firstNameControle() && lastNameControle() && addressControl() && cityControl() && emailControle()) {
+    //Mettre l'objet "contact" dans le local storage :
+        localStorage.setItem("contact", JSON.stringify(contact));
+        sendFromToServer();
+    } 
+    
+    else {
+        alert("❌ Veillez bien remplir le formulaire ❌")
+    }
+    
+    
+    /********************************FIN GESTION DU FORMULAIRE ****************************/
+     
+    // Variable qui récupère l'orderId envoyé comme réponse par le serveur lors de la requête POST :
+    var orderId = "";
+
+    
+    /*******************************REQUÊTE DU SERVEUR ET POST DES DONNÉES *******************/
+    
+    
+    function sendFromToServer () {
+
+        fetch("http://localhost:3000/api/products/order", {
+            method: "POST",
+            body:JSON.stringify({contact, products}) ,
+            headers: {
+                "Content-Type": "application/json",
+            },
+        }) 
+        
+        // Ensuite on stock la réponse de l'api (orderId) :
+        .then((response) => {
+            return response.json();
+        })
+        
+
+        .then((server) => {
+            orderId = server.orderId;
+            // Si la variable orderId n'est pas une chaîne vide on redirige notre utilisateur sur la page confirmation avec la variable :
+            if (orderId != "") {
+                alert("✅ Votre commande à bient était prise en compte ✅");
+                location.href = "confirmation.html?id=" + orderId;
+            }
+        })
+    }
+})
+
+
+/******************************* FIN REQUÊTE DU SERVEUR ET POST DES DONNÉES ***************/
+
---
+++ front/js/product.js
@@ -0,0 +1,244 @@
+
+//Commentaire index.html :
+
+/*<img src="../images/logo.png" alt="Photographie d'un canapé">*/
+/* Nom du produit*/
+/*42*/
+/*Dis enim malesuada risus sapien gravida nulla nisl arcu.*/
+/*<option value="vert">vert</option>
+<option value="blanc">blanc</option>*/
+
+
+//Commentaire Produit n°1 Description :
+
+/*  {
+    "colors": [
+      "Blue",
+      "White",
+      "Black"
+    ],
+    
+    "_id": "107fb5b75607497b96722bda5b504926",
+    "name": "Kanap Sinopé",
+    "price": 1849,
+    "imageUrl": "http://localhost:3000/images/kanap01.jpeg",
+    "description": "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    "altTxt": "Photo d'un canapé bleu, deux places"
+  }, 
+  
+*/
+
+
+
+// URLSearchParams : 
+let url = new URLSearchParams(document.location.search);
+console.log(url);
+let id = url.get("id");
+
+
+//Fonction Fetch :
+fetch('http://localhost:3000/api/products/' + id)
+  .then( (response) => response.json())
+  .then( (dataProducts) => scriptDataProducts (dataProducts));
+
+
+
+  function scriptDataProducts (Kanap) {
+
+    //Création d'une constante imageUrl, qui vas récupérer l'imageUrl des images :
+    const imageUrl = Kanap.imageUrl;
+
+    //Création d'une constante altTxt, qui vas récupérer le altTxt des images :
+    const altTxt = Kanap.altTxt;
+
+    //Création d'une constante name, qui vas récupérer le name des images :
+    const name = Kanap.name;
+
+    //Création d'une constante price, qui vas récupérer le price des images :
+    const price = Kanap.price;
+
+    //Création d'une constante description, qui vas récupérer la description des images :
+    const description = Kanap.description;
+
+    //Création d'une constante colors, qui vas récupérer la colors des images :
+    const colors = Kanap.colors;
+
+
+    
+    
+    //Création d'une constante image, qui vas appelé la fonction makeImageUrl et affectation de imageUrl, altTxt en paramétre:
+    const image = makeImageUrl(imageUrl, altTxt);
+
+    //Création d'une constante h1, qui vas appelé la fonction makeName et affectaton du name en paramétre :
+    const h1 = makeName(name);
+
+    //Création d'une constante prix, qui vas appelé la fonction makePrice et affectaton du price en paramétre :
+    const prix = makePrice(price);    
+
+    //Création d'une constante p, qui vas appelé la fonction makeDescription et affectaton de la description en paramétre :
+    const p = makeDescription(description);
+
+    //Création d'une constante select, qui vas appelé la fonction makeColors et affectaton de la colors en paramétre :
+    const select = makeColors(colors);
+
+    
+    
+
+  function makeImageUrl (imageUrl, altTxt) {
+    //Creation de la balise "<img>" avec insertion du imageUrl et du altTxt :
+    let img = document.createElement('img');
+    img.src = imageUrl;
+    img.alt = altTxt;
+    let parent = document.querySelector('.item__img');
+    parent.append(img);
+      
+  }
+
+
+
+  function makeName (name) {
+    //Selection id = #title et affectation du name :
+      let h1 = document.querySelector('#title');
+      h1.textContent = name; 
+    }
+  
+  
+  
+  
+  function makePrice (price) {
+    //Selection de l'id = #price et affectation du price:
+    let span = document.querySelector('#price');
+    span.textContent = price;
+  }
+  
+  
+  
+
+  function makeDescription (description) {
+    //Selection l'id = #description et affectation de la description :
+    let p = document.querySelector('#description');
+    p.textContent = description;
+  }
+  
+
+  
+  function makeColors (colors) {
+    //Selection l'id = #colors et creation d'une boucle for pour les colors :
+    let select = document.querySelector('#colors');
+    
+    for (let i = 0; i < colors.length ; i++) {
+      let option = document.createElement('option');
+        option.value = colors[i];
+        option.textContent = colors[i];
+        select.append(option);
+    }
+    
+    
+    // Selection de l'id du button et appel de la function addEventListener qui écoute l'évènement click sur le bouton ajouter :
+    
+    const button = document.querySelector("#addToCart");
+    button.addEventListener('click', ajouterAuxPanier);
+
+    function ajouterAuxPanier(e) {
+      
+      e.preventDefault();
+      
+      //Selection des id = #colors et #quantity :
+      let colors = document.querySelector('#colors').value;
+      let quantity = document.querySelector('#quantity').value;
+      
+      
+      //SI la colors ne vaut rien, veuillez choisir une couleur :
+      if(colors == ''){
+            alert('⚠️Veuillez sélectionner une couleur⚠️');
+            return;
+        }
+
+        //SI NON SI la quantity est inférieur à 1 veuillez choisir une quantités valide : 
+        else if (quantity<1){
+            alert('⚠️Veuillez sélectionner le nombre d\'articles souhaités⚠️');
+            return;
+        }
+
+        
+        //SI NON SI la quantity est supérieur à 100 veuillez choisir une quantités entre 1 à 100 produits : 
+        else if (quantity>100){
+          alert('⚠️Vous pouvez seulement sélectionner 1 à 100 produits.⚠️');
+          return;
+        }
+        
+        
+        //SI NON votre commande a bien ete enregistrée :
+        else{
+          alert('✅ Votre article ' + name + ' a bien été ajouté au panier ✅');   
+        }
+        
+        
+        // Enregistrement des valeurs dans un objet optionProduct :
+        const optionProduct = { 
+          id: id,
+          colors: colors,
+          quantity: Number(quantity),
+        }
+
+        
+        //LE LOCALE STORAGE :
+        
+        //Déclaration de la variable "localStorageProducts" dans laquelles on met les key et les values qui sont dans le local stockage :
+        let localStorageProducts = JSON.parse(localStorage.getItem("produits"));
+        
+        
+        //Popup confirmation :
+        const popupConfirmation = () => {
+        
+          //SI OUI alors redirection vers = cart.html :
+        if (confirm("L'article " + name + " à bien été ajouté au panier 🛒, consultez le panier 🆗 ou revenir à la page d'accueil ❌")) {
+          window.location.href = "cart.html";
+        }
+        
+        //SI NON alors redirection vers = index.html :
+        else{
+          window.location.href = "index.html";
+        }
+      }
+      
+      
+      // Si il y a deja des produit dans le locale storage :
+      if (localStorageProducts) {
+        
+        // On rechercher avec la méthode find() si l'id et la couleur d'un article est déjà présent :
+        let item = localStorageProducts.find(
+          (item) =>
+          item.id == optionProduct.id && item.colors == optionProduct.colors
+        );
+          
+          
+          
+          // Si oui on additionne les quantity des articles de même id et colors et mise à jour du localstorageProducts :
+          if (item) {
+            item.quantity = item.quantity + optionProduct.quantity;
+            localStorage.setItem("produits", JSON.stringify(localStorageProducts));
+            popupConfirmation();
+            return;
+        }
+
+        
+        // Si l'article n'est pas déjà dans le local storage alors on push le nouvel article sélectionner :
+        localStorageProducts.push(optionProduct);
+        localStorage.setItem("produits", JSON.stringify(localStorageProducts));
+        popupConfirmation();
+      } 
+      
+      else {
+        //  S'il n'y a pas de produits dans le locale stockage alors création d'un tableau dans le lequel on push l'objet "optionProduct";
+        let newTabLocalStorage = [];
+        newTabLocalStorage.push(optionProduct);
+        localStorage.setItem("produits", JSON.stringify(newTabLocalStorage));
+        popupConfirmation();
+      }
+    }
+  }
+}
+    
+    
+
